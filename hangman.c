868bbfb3307406a713a054cbdd0827d32d93dd71
MuriloBertelli <mrlbertelli@gmail.com>
2023-10-31 12:58:45 -0300

    Update hangman.c

--- hangman.c
+++ hangman.c
@@ -1,12 +1,12 @@
+
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
 #include "hangman.h"
 #include <stdlib.h>
-#define TAMANHO_PALAVRA 20;
 
 // variaveis globais
-char palavrasecreta[TAMANHO_PALAVRA];
+char palavrasecreta[20];
 char chutes[26];
 int chutes_dados = 0;
 
@@ -46,14 +46,20 @@ int jachutou(char letra)
 void desenhaforca()
 
 {
-    printf("  _______      \n");
-    printf(" |/      |     \n");
-    printf(" |      (_)    \n");
-    printf(" |      \\|/   \n");
-    printf(" |       |     \n");
-    printf(" |      / \\   \n");
-    printf(" |             \n");
-    printf("_|___          \n");
+
+    int erros = chuteserrados();
+
+    printf("  _______       \n");
+    printf(" |/      |      \n");
+    printf(" |      %c%c%c  \n", (erros >= 1 ? '(' : ' '),
+           (erros >= 1 ? '_' : ' '), (erros >= 1 ? ')' : ' '));
+    printf(" |      %c%c%c  \n", (erros >= 3 ? '\\' : ' '),
+           (erros >= 2 ? '|' : ' '), (erros >= 3 ? '/' : ' '));
+    printf(" |       %c     \n", (erros >= 2 ? '|' : ' '));
+    printf(" |      %c %c   \n", (erros >= 4 ? '/' : ' '),
+           (erros >= 4 ? '\\' : ' '));
+    printf(" |              \n");
+    printf("_|___           \n");
     printf("\n\n");
 
     for (int i = 0; i < strlen(palavrasecreta); i++)
@@ -81,7 +87,7 @@ void adicionapalavra()
     if (quer == "S")
     {
 
-        char novapalavra[TAMANHO_PALAVRA];
+        char novapalavra[20];
         printf("Qual a nova palavra? ");
         scanf("%s", novapalavra);
 
@@ -143,6 +149,26 @@ int acertou()
     }
     return 1;
 }
+int chuteserrados()
+{
+    int erros = 0;
+    for (int i = 0; i < chutes_dados; i++)
+    {
+        int existe = 0;
+
+        for (int j = 0; j < strlen(palavrasecreta); j++)
+        {
+            if (chutes[i] == palavrasecreta[j])
+            {
+                existe = 1;
+                break;
+            }
+        }
+        if (!existe)
+            erros++;
+    }
+    return erros;
+}
 
 int enforcou()
 {
@@ -163,7 +189,7 @@ int enforcou()
         if (!existe)
             erros++;
     }
-    return erros >= 5;
+    return chuteserrados() >= 5;
 }
 
 int main()
@@ -182,11 +208,40 @@ int main()
 
     if (acertou())
     {
-        printf("Parabens, voce nao e um perdedor");
+        printf("\nParabéns, você ganhou!\n\n");
+
+        printf("       ___________      \n");
+        printf("      '._==_==_=_.'     \n");
+        printf("      .-\\:      /-.    \n");
+        printf("     | (|:.     |) |    \n");
+        printf("      '-|:.     |-'     \n");
+        printf("        \\::.    /      \n");
+        printf("         '::. .'        \n");
+        printf("           ) (          \n");
+        printf("         _.' '._        \n");
+        printf("        '-------'       \n\n");
     }
     else
     {
-        printf("Voce perdeu");
+        printf("\nPuxa, você foi enforcado!\n");
+        printf("A palavra era **%s**\n\n", palavrasecreta);
+
+        printf("    _______________         \n");
+        printf("   /               \\       \n");
+        printf("  /                 \\      \n");
+        printf("//                   \\/\\  \n");
+        printf("\\|   XXXX     XXXX   | /   \n");
+        printf(" |   XXXX     XXXX   |/     \n");
+        printf(" |   XXX       XXX   |      \n");
+        printf(" |                   |      \n");
+        printf(" \\__      XXX      __/     \n");
+        printf("   |\\     XXX     /|       \n");
+        printf("   | |           | |        \n");
+        printf("   | I I I I I I I |        \n");
+        printf("   |  I I I I I I  |        \n");
+        printf("   \\_             _/       \n");
+        printf("     \\_         _/         \n");
+        printf("       \\_______/           \n");
     }
 
     adicionapalavra();
